4ab624def6324c826970f03144f473c45395798c
pbullhove <peterbullhove@gmail.com>
2020-12-16 19:02:15 +0100

    added combi-plot

--- catkin_ws/src/uav_vision/scripts/plot_data_new.py
+++ catkin_ws/src/uav_vision/scripts/plot_data_new.py
@@ -138,6 +138,42 @@ def est_plot_comb(time, gt, yolo, tcv, savename):
     except Exception as e:
         pass
 
+
+def est_plot_comb_filtered(time, gt, yolo, tcv, filtered, savename):
+    file_title = 'yolo_v4_tiny_estimate_vs_gt_hovering'
+    variables = ['x', 'y', 'z', 'yaw']
+    yolo_legend_values = ['est_yolo_x', 'est_yolo_y' ,'est_yolo_z', 'est_yolo_yaw']
+    tcv_legend_values = ['est_tcv_x', 'est_tcv_y' ,'est_tcv_z', 'est_tcv_yaw']
+    filtered_legend_values = ['est_filtered_x', 'est_filtered_y' ,'est_filtered_z', 'est_filtered_yaw']
+    subtitles = variables
+    fig = plt.figure(figsize=(12,8))
+    plt.title('Yolo estimate while hovering')
+    for i in range(4):
+        k = i + 2 if i == 3 else i
+        ax = plt.subplot(2,2,i+1)
+        ax.set_xlabel('Time [s]')
+        ax.set_ylabel('[deg]' if i == 3 else '[m]')
+        ax.axhline(y=0, color='grey', linestyle='--')
+        plt.grid()
+        plt.title(subtitles[i])
+        yolo_line, = ax.plot(time,yolo[:,k], color='b')
+        tcv_line, = ax.plot(time, tcv[:,k], color = 'g')
+        filtered_line, = ax.plot(time, filtered[:,k], color = 'orange')
+        gt_line, = ax.plot(time,gt[:,k], color='r')
+        plt.legend([yolo_line, tcv_line, filtered_line, gt_line],[yolo_legend_values[i],tcv_legend_values[i], filtered_legend_values[i], 'ground truth'])
+
+    folder = './catkin_ws/src/uav_vision/data_storage/plots/'
+    plt.savefig(folder+savename+'.svg')
+
+    fig.tight_layout()
+    # fig.show()
+
+    try:
+        plt.waitforbuttonpress(0)
+        plt.close()
+    except Exception as e:
+        pass
+
 def main():
     for file in os.listdir("./catkin_ws/src/uav_vision/data_storage"):
         try:
@@ -150,14 +186,15 @@ def main():
         if savename.split('_')[-1] == 'comb':
             est_plot_comb(time, gt, yolo, tcv, savename + "_var_plot")
             est_plot(time, gt, filtered, savename + "_filtered_var_plot")
+            est_plot_comb_filtered(time, gt, yolo, tcv, filtered, savename + "_comb_plot")
         elif savename.split('_')[-1] == 'yolo':
-            est_plot(time, gt, yolo, savename +"_var_plot")
+            est_plot(time, gt, filtered, savename +"_filtered_plot")
         elif savename.split('_')[-1] == 'tcv':
-            est_plot(time, gt, yolo, savename + "_var_plot")
+            est_plot(time, gt, filtered, savename + "_var_plot")
         else:
             est_plot_comb(time, gt, yolo, tcv, savename + "_var_plot")
             est_plot(time, gt, filtered, savename + "_filtered_var_plot")
-        if savename.split('_')[0] == 'landing':
+        if savename.split('_')[0] in ['landing','land']:
             plot_xy(time, gt, filtered, savename+"_xy_plot")
             plot_xyz(time, gt, filtered, savename+"_xyz_plot")
 
